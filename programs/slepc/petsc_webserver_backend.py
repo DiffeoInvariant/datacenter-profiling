9131b64f6c732595a67744529de81ea195787662
DiffeoInvariant <zane.jakobs@colorado.edu>
2020-11-25 09:43:53 -0800

    python backend

--- programs/slepc/petsc_webserver_backend.py
+++ programs/slepc/petsc_webserver_backend.py
@@ -3,10 +3,14 @@ import sys
 from flask import Flask, request
 import re
 import math
+import time
+import argparse
 
 app = Flask(__name__)
 
 entries = {}
+
+datafile = '/opt/tcpsummary'
 
 def read_file(filename):
     lines = open(filename,'r').readlines()
@@ -58,11 +62,54 @@ def read_file(filename):
                                             avg_lat,avg_life,fraction_ipv6)}
         i += 1
 
-#@app.route('/')
-#def 
+
+
+def format_entries():
+    def format_entry(entry):
+        ipentry = f"{(100 * entry[6]):2.2f} " if entry[6] == 0.0 else f"{(100 * entry[6]):2.2f}"
+        fstr = f"""
+_________________________________________________________________
+| Summary of network traffic on MPI rank {rk:3d} with PID {pid:8d}: |
+|        process name............... = {entry[0]:15s}          |
+|        transmitted kB............. = {entry[1]:8d}                 |
+|        received kB................ = {entry[2]:8d}                 |
+|        num TCP events............. = {entry[3]:8d}                 |
+|        Average connection latency  = {entry[4]:8.2f}                 |
+|        Average connection lifetime = {entry[5]:8.2f}                 |
+|        Percent IPv6............... = {ipentry}                    |
+|_______________________________________________________________|
+        """
+        return fstr
+
+    entry_ls = []
+    for rk in entries:
+        rk_entries = []
+        #print(f"entries[{rk}] = {entries[rk]}")
+        for pid in entries[rk]:
+            rk_entries.append(format_entry(entries[rk][pid]))
+        entry_ls.append('\n'.join(rk_entries))
+
+    return '\n'.join(entry_ls)
 
 
 
-read_file('example.out')
+        
+@app.route('/')
+def root():
+    read_file(datafile)
+    return format_entries()
+    
+    
 
-print(f"entries is \n{entries}")
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-f','--file',default='/opt/tcpsummary',help='Where is the PETSc webserver (program webserver) dumping its output to?')
+    parser.add_argument('-p','--port',type=int,default=5000,help='Which port to run Flask on?')
+    parser.add_argument('--no_flask',action='store_true')
+    args = parser.parse_args()
+    datafile = args.file
+    if args.no_flask:
+        read_file(datafile)
+        print(format_entries())
+    else:
+        app.run(host='0.0.0.0',port=args.port)
