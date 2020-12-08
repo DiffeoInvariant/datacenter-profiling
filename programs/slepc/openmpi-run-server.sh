7e6aed440cac6f73e75c9205497a2b295aa70bb4
Emily Jakobs <emilycjakobs@gmail.com>
2020-12-08 07:15:33 -0800

    shell script

--- programs/slepc/openmpi-run-server.sh
+++ programs/slepc/openmpi-run-server.sh
@@ -18,4 +18,4 @@ done
 shift $((OPTIND-1))
 	    
 
-mpirun -n ${n} --map-by node ./webserver --accept_file $ACCEPT_DATA --connect_file $CONNECT_DATA --connlat_file $CONNLAT_DATA --life_file $LIFE_DATA
+mpirun -n ${n} --map-by node --hostfile hostfile ./webserver --accept_file $ACCEPT_DATA --connect_file $CONNECT_DATA --connlat_file $CONNLAT_DATA --life_file $LIFE_DATA
