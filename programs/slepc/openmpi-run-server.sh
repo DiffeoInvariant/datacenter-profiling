f817aba66647eed9e239eafe259e479f359b4da9
Emily Jakobs <emilycjakobs@gmail.com>
2020-12-08 07:08:51 -0800

    shell script

---
+++ programs/slepc/openmpi-run-server.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+usage() { echo "Usage: $0 [-n <int (num nodes)>] [-o <string (output file)]" 1>&2; exit 1; }
+
+n=2
+out=$HOME/tcpdata.log
+
+while getopts ":n:o:" opt; do
+    case "${opt}" in
+	n)
+	    n=${OPTARG}
+	    ;;
+	o)
+	    out=${OPTARG}
+	    ;;
+    esac
+done
+shift $((OPTIND-1))
+	    
+
+mpirun -n ${n} --map-by node ./webserver --accept_file $ACCEPT_DATA --connect_file $CONNECT_DATA --connlat_file $CONNLAT_DATA --life_file $LIFE_DATA
